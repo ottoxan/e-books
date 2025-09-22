763a5f056d77ab40c2d521cb39e689e60748bf52
hilmiyahya04 <mashh101004@gmail.com>
2025-09-22 19:58:30 +0700

    h

--- localhost.sql
+++ localhost.sql
@@ -20,8 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `e-books`
 --
-CREATE DATABASE IF NOT EXISTS `e-books` DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
-USE `e-books`;
+-- CREATE DATABASE IF NOT EXISTS `e-books` DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
+-- USE `e-books`;
 
 -- --------------------------------------------------------
 
